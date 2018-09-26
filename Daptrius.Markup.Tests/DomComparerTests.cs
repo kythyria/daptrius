317de41dbff4040a045fbb71b9cf27c871519fbc
Kythyria Tieran <kythyria@berigora.net>
2018-09-26 18:39:52 +0100

    Add thing to compare DOMs and some tests for it.

---
+++ Daptrius.Markup.Tests/DomComparerTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.IO;
+using System.Xml;
+using System.Xml.XPath;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Daptrius.Markup.Tests
+{
+    [System.AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    sealed class FileDataAttribute : Attribute, ITestDataSource {
+
+        // This is a positional argument
+        public FileDataAttribute(string filename) {
+            Filename = filename;
+        }
+
+        public string Filename { get; }
+
+        public IEnumerable<object[]> GetData(MethodInfo methodInfo) {
+            var doc = new XmlDocument();
+
+            var xrs = new XmlReaderSettings {
+                IgnoreWhitespace = false,
+            };
+            using (var tr = new StreamReader(Filename))
+            using (var xr = XmlReader.Create(tr)) {
+                doc.Load(xr);
+            }
+
+            var xnm = new XmlNamespaceManager(doc.NameTable);
+            xnm.AddNamespace("t", "https://ns.berigora.net/2018/Daptrius/TestDefinitions");
+
+            Func<XmlNode, bool> SignificantNode = k => {
+                if (k is XmlCharacterData kc) {
+                    return string.IsNullOrWhiteSpace(kc.Data);
+                }
+                else { return true; }
+            };
+
+            foreach (XmlNode i in doc.SelectNodes("/t:test", xnm)) {
+                var nr = new object[] { i.Attributes["name"].Value, i.Attributes["result"].Value };
+                var items = i.SelectNodes("t:item", xnm).Cast<XmlElement>()
+                    .Select(j => (object)j.ChildNodes
+                        .Cast<XmlNode>()
+                        .First(SignificantNode));
+                yield return nr.Concat(items).ToArray();
+            }
+        }
+
+        public string GetDisplayName(MethodInfo methodInfo, object[] data) {
+            return data[0] as string;
+        }
+    }
+
+    [TestClass]
+    public class DomComparerTests
+    {
+        [TestMethod]
+        [FileData("DomComparerTests.xml")]
+        public void TestDom(string name, string result, XmlNode left, XmlNode right) {
+            var ld = new XmlDocument();
+            var rd = new XmlDocument();
+
+            ld.LoadXml((left as XmlCharacterData).Data);
+            rd.LoadXml((right as XmlCharacterData).Data);
+
+            var comp = new XmlDomComparer();
+
+            Assert.IsTrue(comp.Equals(ld, rd));
+        }
+    }
+}
---
+++ Daptrius.Markup.Tests/XmlDomComparer.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Linq;
+
+namespace Daptrius.Markup.Tests
+{
+    /// <summary>
+    /// Compares XML documents, in a namespace aware manner.
+    /// </summary>
+    /// <remarks>
+    /// Insignificant whitespace counts. Namespace declarations and prefixes don't.
+    /// Entity references aren't expanded, and their replacement text isn't counted.
+    /// Nothing inside an internal or external subset counts.
+    /// </remarks>
+    class XmlDomComparer
+    {
+        public IEnumerable<XmlNode> Children(XmlNode parent) {
+            return parent.ChildNodes.Cast<XmlNode>();
+        }
+
+        public bool Equals(XmlNode x, XmlNode y) {
+            if (x.GetType() != y.GetType()) {
+                return false;
+            }
+            else if (x is XmlCharacterData xcd) {
+                return Equals(xcd, y as XmlCharacterData);
+            }
+            else if (x is XmlDeclaration xd) {
+                return Equals(xd, y as XmlDeclaration);
+            }
+            else if (x is XmlDocumentType xdt) {
+                return Equals(xdt, y as XmlDocumentType);
+            }
+            else if (x is XmlProcessingInstruction xpi) {
+                return Equals(xpi, y as XmlProcessingInstruction);
+            }
+            else if (x is XmlEntityReference xer) {
+                return Equals(xer, y as XmlEntityReference);
+            }
+            else if (x is XmlElement xe) {
+                return Equals(xe, y as XmlElement);
+            }
+            else if (x is XmlDocument xdoc) {
+                return Equals(xdoc, y as XmlDocument);
+            }
+            else if (x is XmlDocumentFragment xdf) {
+                return Equals(xdf, y as XmlDocumentFragment);
+            }
+            else {
+                throw new ArgumentException("Unknown XmlNode subclass");
+            }
+        }
+
+        public bool Equals(XmlCharacterData x, XmlCharacterData y) {
+            return x.Data == y.Data;
+        }
+
+        public bool Equals(XmlDeclaration x, XmlDeclaration y) {
+            return x.Encoding == y.Encoding && x.Version == y.Version && x.Standalone == y.Standalone;
+        }
+
+        public bool Equals(XmlDocumentType x, XmlDocumentType y) {
+            return x.Name == y.Name && x.PublicId == y.PublicId && x.SystemId == y.SystemId;
+        }
+
+        public bool Equals(XmlProcessingInstruction x, XmlProcessingInstruction y) {
+            return x.Target == y.Target && x.Data == y.Data;
+        }
+
+        public bool Equals(XmlEntityReference x, XmlEntityReference y) {
+            return x.Name == y.Name;
+        }
+
+        public bool Equals(XmlElement x, XmlElement y) {
+            if (x.LocalName != y.LocalName) { return false; }
+            if (x.NamespaceURI != y.NamespaceURI) { return false; }
+
+            var xattrs = x.Attributes.Cast<XmlAttribute>().Where(i => !IsNsDecl(i)).OrderBy(i => $"{i.NamespaceURI}:{i.LocalName}");
+            var yattrs = y.Attributes.Cast<XmlAttribute>().Where(i => !IsNsDecl(i)).OrderBy(i => $"{i.NamespaceURI}:{i.LocalName}");
+
+            if(!xattrs.Zip(yattrs, AttrEqual).Any(i=>i)) { return false; }
+
+            return ChildrenEquals(x, y);
+        }
+
+        public bool Equals(XmlDocument x, XmlDocument y) => ChildrenEquals(x, y);
+        public bool Equals(XmlDocumentFragment x, XmlDocumentFragment y) => ChildrenEquals(x, y);
+
+        private bool AttrEqual(XmlAttribute x, XmlAttribute y) {
+            return x.NamespaceURI == y.NamespaceURI && x.LocalName == y.LocalName && x.Value == y.Value;
+        }
+
+        private bool ChildrenEquals(XmlNode x, XmlNode y) {
+            return x.ChildNodes.Cast<XmlNode>().Zip(y.Cast<XmlNode>(), Equals).Any(i => i);
+        }
+
+        private bool IsNsDecl(XmlAttribute attr) {
+            return attr.Prefix == "xmlns" || (attr.Prefix == "" && attr.LocalName == "xmlns");
+        }
+    }
+}
