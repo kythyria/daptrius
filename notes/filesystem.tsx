c8c78967e544f89c07e5428b306968e353f1e1cd
Kythyria Tieran <kythyria@berigora.net>
2019-10-20 10:05:03 +0100

    We could use git instead of postgres

--- notes/filesystem.tsx
+++ notes/filesystem.tsx
@@ -1,85 +1,65 @@
 type AsyncResult<T, E extends Error = Error> = Promise<T|E>;
+import * as stream from 'stream';
+
+type Principal = string;
 
 interface Volume {
-    name: string;
-    type: string;
-    getInode(what: string | number) : AsyncResult<Inode>;
-    getPath(what: string | string[]) : AsyncResult<Inode>;
+    label : string;
+    type  : "wiki";
+    rootInode : Promise<Inode>;
+    resolvePath(path: string) : Promise<Inode>;
+    resolveInode(inodeId: string) : Promise<Inode>;
 };
 
-type attributeType = "data" | "keyvalues" | "index" | "entries" | "atom";
+interface UnlockedVolume extends Volume {
+    beginNewVersion(principal: Principal) : Promise<WriteableVolume>;
+}
+
+interface WriteableVolume extends Volume {
+    commit() : Promise<void>;
+    abandon() : Promise<void>;
+
+    newInode() : Promise<WriteableInode>;
+
+    rootInode : Promise<WriteableInode>;
+    resolvePath(path: string) : Promise<WriteableInode>;
+    resolveInode(inodeId: string) : Promise<WriteableInode>;
+}
 
 interface Inode {
-    id: number;
-    attribute(name: string, expectedType: attributeType) : Attribute,
-    renameAttribute(oldName: string, newName: string) : Promise<void>,
-    removeAttribute(attr: string) : Promise<void>,
+    id: string;
+    title: string;
+    canonicalPath: string;
+    getAttribute(name: string) : Promise<Attribute>;
+    traverse(index: string, slug: string) : Promise<Inode>;
+};
+
+interface WriteableInode extends Inode {
+    putAttribute(name: string, newMime: string, newData : any) : Promise<void>;
+    traverse(index: string, slug: string) : Promise<WriteableInode>;
+    unlinkChild(index: string, slug: string) : Promise<void>;
+    link(index: string, newLink: LinkInfo) : Promise<void>;
+}
+
+interface LinkInfo {
+    target: {volume?: string, inode: string} | {url: string},
+    sortKey: string,
+    slug: string,
+    type: "redirect" | "hardlink"
+}
+
+interface RedirectLinkInfo extends LinkInfo {
+    type: "redirect",
+    target: {volume?: string, inode: string} | {url: string},
+}
+
+interface HardLinkInfo extends LinkInfo {
+    type: "hardlink",
+    target: {volume?: string, inode: string}
 }
 
 interface Attribute {
-    type: attributeType;
-}
-
-interface AttributeCoreStats {
     name: string;
-    type: attributeType;
-    size: number;
-    contentType: string;
-    modified: Date;
-    etag?: string;
-}
-
-type AttributeResponse<T> = AttributeCoreStats & {stream: T};
-
-interface DataAttribute extends Attribute {
-    type: "data",
-    read(start: number, length?: number) : AsyncResult<AttributeResponse<ReadableStream>>;
-    replace(data: Buffer|string|ReadableStream, newType?: string) : AsyncResult<void>;
-    patch(data: Buffer|string|ReadableStream, newType?: string) : AsyncResult<void>;
-    stat() : AsyncResult<AttributeCoreStats>;
-}
-
-interface AtomAttribute extends Attribute {
-    type: "atom",
-    read(start: number, length?: number) : AsyncResult<AttributeResponse<ReadableStream>>;
-    replace(data: Buffer|string|ReadableStream, newType?: string) : AsyncResult<void>;
-    stat() : AsyncResult<AttributeCoreStats>;
-}
-
-interface KeyvaluesAttribute extends Attribute {
-    type: "keyvalues",
-    read(items?: string[]) : AsyncResult<AttributeResponse<KeyvaluesStream>>;
-    replace(newitems: KeyvaluesStream) : AsyncResult<void>;
-    patch(newitems: KeyvaluesStream) : AsyncResult<void>;
-}
-
-type KeyvaluesStream = AsyncIterableIterator<[string, any][]>;
-
-interface IndexAttribute extends Attribute {
-    type: "index" | "entries",
-    read(start: number, length?: number) : AsyncResult<AttributeResponse<AsyncIterableIterator<IndexItem>>>;
-    patch(newitems: AsyncIterableIterator<IndexOp>) : AsyncResult<void>;
-}
-
-type IndexOp = {op: "link", item: Omit<IndexItem, "id">} | {op: "overwrite", item: Partial<IndexItem>} | {op: "unlink", id: number};
-
-type IndexItem = InodeIndexItem | UrlIndexItem;
-
-interface BaseIndexItem {
-    type: "hard" | "soft" | "url";
-    id: number;
-    slug: string;
-    container: { volume: string, inode: number };
-    sortKey: string;
-    canonical: boolean;
-}
-
-interface InodeIndexItem extends BaseIndexItem {
-    type: "hard"|"soft",
-    target: { volume: string, inode: number };
-}
-
-interface UrlIndexItem extends BaseIndexItem {
-    type: "url",
-    target: string
+    mime: string;
+    contents: stream.Readable;
 }
